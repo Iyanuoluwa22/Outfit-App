c2f48aa64bb31aee10bd0580a7b9c8fbacf0b5a9
kaveh <kaveh.samimi1@gmail.com>
2024-04-20 15:19:50 -0400

    Created the skeleton of the for-you page to have posts, a top bar, and background colors.

--- app/src/main/java/edu/towson/outfitapp/MainPage/ForYouPage.kt
+++ app/src/main/java/edu/towson/outfitapp/MainPage/ForYouPage.kt
@@ -1,5 +1,6 @@
 package edu.towson.outfitapp.MainPage
 
+import androidx.compose.foundation.background
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
@@ -7,6 +8,12 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.modifier.modifierLocalConsumer
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
 
 // classes of data that we are going to use to show user info before we have the database ...
 data class User(
@@ -24,14 +31,67 @@ data class Post(
 object DummyData {
     val Users = listOf(
         User(1, "Kevin"),
-        User(1, "Nu"),
-        User(1, "Christian")
+        User(2, "Nu"),
+        User(3, "Christian")
     )
+
+    val Posts = listOf(
+        Post(1, "Developing some Apps today!", 1),
+        Post(2, "The weather is pretty good today.", 2),
+        Post(3, "NBA Playoffs are starting this weekend!", 2)
+    )
+}
+
+@Preview
+@Composable
+fun PagePreview(){
+    MainPage(CurruserId = 3)
+}
+
+@Composable
+fun TopBar(){
+    Surface(
+        modifier = Modifier.fillMaxWidth()
+    ){
+        Text(text = "Top Bar", modifier = Modifier.padding(15.dp))
+    }
 }
 
 
 @Composable
-fun MainPage (userId : Int){ // User ID passed is the user that is currently logged into the system.
+fun MainPage (CurruserId : Int){ // User ID passed is the user that is currently logged into the system.
+    // show all of the posts that are not made by the current user.
+    val ShownPosts = DummyData.Posts.filter { it.PostingUserId != CurruserId }
 
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Cyan)
+    ){
+        TopBar()
+        LazyColumn(
+            modifier = Modifier
+                .weight(1f)
+                .padding(10.dp)
+        ){
+            items(ShownPosts) { post ->
+                Posted(post)
+                Spacer(modifier = Modifier.height(10.dp))
+            }
+        }
+    }
+
+}
+
+@Composable
+fun Posted(post: Post){
+    Surface(
+      modifier = Modifier.fillMaxWidth(), color = Color.LightGray
+    ){
+        Text(
+            text = post.Caption,
+            modifier = Modifier.padding(15.dp)
+        )
+    }
 }
 
